a4236fb0ae7e0c073e4e09923cc80621a46dd9f5
codesignbytom <graafmans.tom@gmail.com>
2024-02-05 23:10:44 +0100

    changed 5 files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "production",
@@ -8,6 +9,15 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -30,6 +40,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
     }),
   ],
   optimization: {
