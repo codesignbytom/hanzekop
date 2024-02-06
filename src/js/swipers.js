95d50277b10b256ba01ffbeefd94d4f1b7ff57e9
codesignbytom <graafmans.tom@gmail.com>
2024-02-06 14:42:53 +0100

    modified swipers.js in js

--- src/js/swipers.js
+++ src/js/swipers.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const homesSlider = new Swiper(".swiper.is-home-preview", {
     loop: true,
     slidesPerView: 1,
+    spaceBetween: 16,
     centeredSlides: true,
     speed: 800,
     grabCursor: true,
@@ -11,18 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
     navigation: {
       nextEl: ".section_home-previews .swiper-btn-next",
       prevEl: ".section_home-previews .swiper-btn-prev",
-    },
-    pagination: {
-      el: ".section_home-previews .swiper-pagination",
-      type: "fraction",
-    },
-    breakpoints: {
-      480: {
-        slidesPerView: 1.5,
-      },
-      768: {
-        slidesPerView: 2,
-      },
     },
   });
 
@@ -38,26 +27,18 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".section_testimonials .swiper-btn-next",
       prevEl: ".section_testimonials .swiper-btn-prev",
     },
-    pagination: {
-      el: ".section_testimonials .swiper-pagination",
-      type: "fraction",
-    },
     breakpoints: {
       480: {
         slidesPerView: 1.5,
-        spaceBetween: 16,
       },
       768: {
         slidesPerView: 1.75,
-        spaceBetween: 32,
       },
       992: {
         slidesPerView: 1.75,
-        spaceBetween: 48,
       },
       1442: {
         slidesPerView: 1.75,
-        spaceBetween: 64,
       },
     },
   });
